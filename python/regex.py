ce49e5a0b17973df07759b93b729da66bb1cd1df
Turtle lea <leandro.matayoshi@gmail.com>
2015-04-25 15:51:58 -0300

    Adding .gitignore. Adding class Regex

---
+++ python/regex.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+class Regex:
+  def __init__(self, nombre, caso_base, valor, unario, cant_argumentos, argumentos ):
+    self.nombre = nombre
+    self.caso_base = caso_base
+    self.unario = unario
+    self.cant_argumentos = cant_argumentos
+    self.argumentos = argumentos
