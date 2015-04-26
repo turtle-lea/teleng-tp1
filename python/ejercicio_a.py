09c0830d2106b11349a51b5f40264f3ee33847fa
Leandro Vega <leandrovega@ubuntu.(none)>
2015-04-26 00:41:59 -0300

    estructura del automata

--- python/ejercicio_a.py
+++ python/ejercicio_a.py
@@ -4,3 +4,12 @@
 
 def afd_minimo(archivo_regex, archivo_automata):
     raise NotImplementedError
+
+
+class Automata:
+  def __init__(self, estados, alfabeto, estado_inicial, estados_finales, transiciones):
+    self.estados = estados
+    self.alfabeto = alfabeto
+    self.estado_inicial = estado_inicial
+    self.estados_finales = estados_finales
+    self.transiciones = transiciones
