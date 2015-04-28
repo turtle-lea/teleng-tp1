251bb8c7272b44f2d6f411be15786c69ffe1b17f
Ignacio Niesz <ignacio.niesz@gmail.com>
2015-04-27 21:54:49 -0300

    ejercicio_a minimizacion

--- python/ejercicio_a.py
+++ python/ejercicio_a.py
@@ -12,6 +12,54 @@ def afd_minimo(archivo_regex, archivo_automata):
 	escribir_archivo(automata, archivo_automata)
 
 	return automata
+	
+
+def transitions_group(automata):
+	group = {}
+	for transition in automata.transitions:
+		# key == (qi, qj)
+		key = transition[0], transition[2]
+		# value == [label1, label2, ...]
+		value = []
+		if key in group:
+			value = group[key]
+			if not(transition[1] in value):
+				value.append(transition[1])
+				
+		group[key] = value
+		
+	return group	
+
+# http://www.cs.odu.edu/~toida/nerzic/390teched/regular/fa/min-fa.html
+def minimizar_afd(automata):
+	s1 = automata.finales
+	s2 = [estado for estado in automata.estados if estado not in s1] 
+	partition = [s1, s2]
+	
+	transitions = transitions_group(automata)
+	new_partition = particionar(partition, transitions)
+	
+	while not (new_partition == partition):
+		new_partition = particionar(partition)
+		
+	partition = eliminar_estados_trampa(new_partition)
+	
+	
+def nueva_particion (partition, transitions):
+	
+def particionar (subconjunto, transitions):
+	resultado = []
+	
+	for elemento in subconjunto:
+		i = 0
+		encontrado = false
+		while (i < len(subconjunto)) and (not encontrado):
+			encontrado = (elemento, subconjunto[i]) in transitions
+		
+		if ( 
+	
+
+
 
 def escribir_archivo(automata, filename):
 	f = open(filename, 'w')
