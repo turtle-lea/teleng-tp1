b7c188719edb524cbeea5c6f1509dc40ba57cc8a
Leandro Vega <leandrovega@ubuntu.(none)>
2015-04-29 05:26:04 -0300

    arme los archivos, ejercicio_b,_d,_e

--- python/ejercicio_e.py
+++ python/ejercicio_e.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*- 
 #!/usr/bin/python
 
+from automata import *
 
 def complemento(archivo_automata1, archivo_automata):
-    raise NotImplementedError
+	automata1 = parsear_automata(archivo_automata1)
+	automata2 = automata1.complemento()
+	escribir_archivo(automata2,archivo_automata)
+	return 0
