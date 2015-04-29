ad2d54df5b6b7fe6f570cbfdd59a7389eed97489
NIEN08 <ignacio.niesz@gmail.com>
2015-04-29 04:48:20 -0300

    minimizar_afd

---
+++ python/ejemplo_min_afdx.py
@@ -0,0 +1,123 @@
+from ejercicio_a import *
+
+def test1():
+    transiciones = []
+    transiciones.append([1, 'a', 3])
+    transiciones.append([1, 'b', 2])
+    transiciones.append([2, 'a', 4])
+    transiciones.append([2, 'b', 1])
+    transiciones.append([3, 'a', 5])
+    transiciones.append([3, 'b', 4])
+    transiciones.append([4, 'a', 4])
+    transiciones.append([4, 'b', 4])
+    transiciones.append([5, 'a', 3])
+    transiciones.append([5, 'b', 2])
+    alfabeto = ["a", "b"]
+    estados = [1,2,3,4,5]
+    estados_finales = [1,5]
+    estado_inicial = 1
+
+    automat = Automata (estados, alfabeto, estado_inicial, estados_finales, transiciones)
+    min_afd = minimizar_afd(automat)
+
+    #dicctest = transitions_by_state_and_label(automat.transiciones)
+    #print (dicctest[(3, 'b')])
+    
+    print (min_afd.estados)
+    print (min_afd.alfabeto)
+    print (min_afd.estado_inicial)
+    print (min_afd.estados_finales)
+    print (min_afd.transiciones)
+
+def test2():
+    transiciones = []
+    alfabeto = []
+    estados = [1]
+    estados_finales = [1]
+    estado_inicial = 1
+    automat = Automata (estados, alfabeto, estado_inicial, estados_finales, transiciones)
+    min_afd = minimizar_afd(automat)
+    print (min_afd.estados)
+    print (min_afd.alfabeto)
+    print (min_afd.estado_inicial)
+    print (min_afd.estados_finales)
+    print (min_afd.transiciones)
+
+def test3():
+    transiciones = []
+    transiciones.append([1, 'a', 2])
+    transiciones.append([2, 'b', 3])
+    transiciones.append([3, 'c', 4])
+    transiciones.append([4, 'd', 5])
+    alfabeto = ["a", "b", "c", "d"]
+    estados = [1,2,3,4,5]
+    estados_finales = [5]
+    estado_inicial = 1
+
+    automat = Automata (estados, alfabeto, estado_inicial, estados_finales, transiciones)
+    min_afd = minimizar_afd(automat)
+
+    #dicctest = transitions_by_state_and_label(automat.transiciones)
+    #print (dicctest[(3, 'b')])
+    
+    print (min_afd.estados)
+    print (min_afd.alfabeto)
+    print (min_afd.estado_inicial)
+    print (min_afd.estados_finales)
+    print (min_afd.transiciones)
+
+def test4():
+    transiciones = []
+    transiciones.append([1, 'a', 2])
+    transiciones.append([1, 'b', 3])
+    transiciones.append([2, 'a', 2])
+    transiciones.append([2, 'b', 4])
+    transiciones.append([3, 'a', 3])
+    transiciones.append([3, 'b', 3])
+    transiciones.append([4, 'a', 6])
+    transiciones.append([4, 'b', 3])
+    transiciones.append([5, 'a', 5])
+    transiciones.append([5, 'b', 3])
+    transiciones.append([6, 'a', 5])
+    transiciones.append([6, 'b', 4])
+    alfabeto = ["a", "b"]
+    estados = [1,2,3,4,5,6]
+    estados_finales = [1,2,4,5,6]
+    estado_inicial = 1
+
+    automat = Automata (estados, alfabeto, estado_inicial, estados_finales, transiciones)
+    min_afd = minimizar_afd(automat)
+
+    #dicctest = transitions_by_state_and_label(automat.transiciones)
+    #print (dicctest[(3, 'b')])
+    
+    print (min_afd.estados)
+    print (min_afd.alfabeto)
+    print (min_afd.estado_inicial)
+    print (min_afd.estados_finales)
+    print (min_afd.transiciones)
+
+def test5():
+    transiciones = []
+    transiciones.append([0, '0', 1])
+    transiciones.append([0, '1', 0])
+    transiciones.append([1, '0', 0])
+    transiciones.append([1, '1', 1])
+    transiciones.append([2, '1', 0])
+    transiciones.append([2, '0', 1])
+    alfabeto = ["0", "1"]
+    estados = [0,1,2]
+    estados_finales = [1]
+    estado_inicial = 0
+
+    automat = Automata (estados, alfabeto, estado_inicial, estados_finales, transiciones)
+    min_afd = minimizar_afd(automat)
+
+    #dicctest = transitions_by_state_and_label(automat.transiciones)
+    #print (dicctest[(3, 'b')])
+    
+    print (min_afd.estados)
+    print (min_afd.alfabeto)
+    print (min_afd.estado_inicial)
+    print (min_afd.estados_finales)
+    print (min_afd.transiciones)
--- python/ejercicio_a.py
+++ python/ejercicio_a.py
@@ -5,122 +5,328 @@ from regex import *
 from automata import *
 
 def afd_minimo(archivo_regex, archivo_automata):
+        regpars = parse_regex(archivo_regex)
+        automata = armar_automata(regpars)
+        #automata = automata.remover_transiciones_lambda()
+        #escribir_archivo(automata, archivo_automata)
 
-	regpars = parse_regex(archivo_regex)
-	automata = armar_automata(regpars)
-	#automata = automata.remover_transiciones_lambda()
-	#escribir_archivo(automata, archivo_automata)
+        return automata
 
-	return automata
-	
 
-def transitions_group(automata):
-	group = {}
-	for transition in automata.transitions:
-		# key == (qi, qj)
-		key = transition[0], transition[2]
-		# value == [label1, label2, ...]
-		value = []
-		if key in group:
-			value = group[key]
-			if not(transition[1] in value):
-				value.append(transition[1])
-				
-		group[key] = value
-		
-	return group	
+#automata debe ser deterministico
+def transitions_by_state_and_label(automata):
+        group = {}
+        for transition in automata.transiciones:
+                #key = (origin_state, label)
+                key = transition[0], transition[1]
+                #value = dest_state (deterministic automat goes to one state)
+                value = transition[2]
+                group[key] = value
+
+        return group
 
 # http://www.cs.odu.edu/~toida/nerzic/390teched/regular/fa/min-fa.html
 def minimizar_afd(automata):
-	s1 = automata.finales
-	s2 = [estado for estado in automata.estados if estado not in s1] 
-	partition = [s1, s2]
-	
-	transitions = transitions_group(automata)
-	new_partition = particionar(partition, transitions)
-	
-	while not (new_partition == partition):
-		new_partition = particionar(partition)
-		
-	partition = eliminar_estados_trampa(new_partition)
-	
-	
-def nueva_particion (partition, transitions):
-	
-def particionar (subconjunto, transitions):
-	resultado = []
-	
-	for elemento in subconjunto:
-		i = 0
-		encontrado = false
-		while (i < len(subconjunto)) and (not encontrado):
-			encontrado = (elemento, subconjunto[i]) in transitions
-		
-		if ( 
-	
+        #transitions = {(1, "a"): 3, (1, "b"): 2, (2, "a"): 4, (2, "b"): 1, (3, "a"): 5, (3, "b"): 4, (4, "a"): 4, (4, "b"): 4, (5, "a"): 3, (5, "b"): 2}
+        #alphabet = ["a", "b"]
+        #partition = [[1, 5], [2, 3, 4]]
+        transitions = transitions_by_state_and_label(automata)
+        s1 = [state for state in automata.estados_finales]
+        s2 = [state for state in automata.estados if state not in s1]
+        partition = [s1, s2]
+        new_partition = nueva_particion(partition, automata.alfabeto, transitions)
+        while not (new_partition == partition):
+                print(partition)
+                print(new_partition)
+                partition = new_partition
+                new_partition = nueva_particion(partition, automata.alfabeto, transitions)
+
+
+        #Delete "trap" states from the new partition
+        partition = []
+        for subset in new_partition:
+                print (subset)
+                new_subset = eliminate_death_states(subset, automata.alfabeto, transitions, automata.estados_finales)
+                print (new_subset)
+                if len(new_subset) > 0:
+                        partition.append(new_subset)
+
+        #Generate the reduce automat transitions
+        transitions_dicc = partition_transitions(partition, automata.alfabeto, transitions)
+        new_transitions = []
+        new_states = []
+        for key, value in transitions_dicc.items():
+                #transition == (orig, dest)
+                #labels == (label1, label2, label3)
+                orig_state = key[0]
+                label = key[1]
+                dest_state = value
+                new_transitions.append([orig_state, label, dest_state])
+                if not(orig_state in new_states):
+                    new_states.append(orig_state)
+                if not(dest_state in new_states):
+                    new_states.append(dest_state)
+        new_states.sort()
+        
+        #Generate min-automat init state
+        new_init_state = choose_representatives(partition, automata.estado_inicial)
+
+        #Generate min-automat final states
+        new_final_states = []
+        #print("automata.estados_finales" + str(automata.estados_finales))
+        for final in automata.estados_finales:
+            new_final = choose_representatives(partition, final)
+            if not(new_final in new_final_states):
+                new_final_states.append(new_final)
+
+
+        min_afd = Automata(new_states, automata.alfabeto, new_init_state, new_final_states, new_transitions)
+        return min_afd
+
+
+
+def nueva_particion (partition, alphabet, transitions):
+    #print(partition)
+    new_partition = partition
+    for symbol in alphabet:
+        #print ("symbol: " + str(symbol))
+        condition1 = True
+        while condition1:
+            partition = new_partition
+            index = 0
+            condition2 = (index < len(partition))
+            
+            while condition2:    
+                subset = partition[index]
+                prefix_partition = partition[0:index]
+                suffix_partition = partition[index+1:len(partition)]
+                disjoint_subset  = disjoin(prefix_partition, subset, suffix_partition, symbol, transitions)
+                index += 1
+                condition2 = ([subset] == disjoint_subset and index < len(partition))
+                
+            new_partition = prefix_partition + disjoint_subset + suffix_partition
+            condition1 = not (new_partition == partition)
+            #print("partition: " + str(partition))
+            #print("new_partition: " + str(new_partition))
+
+       
+
+    return new_partition
+
+
+
+def disjoin(prefix_set, subset, suffix_set, symbol, transitions):
+    aux_partition = []
+    states_by_representative = {}
+    #print("====== INPUT ======")
+    #print("symbol: " + symbol)
+    #print("transitions: " + str(transitions))
+    #print("prefix: " + str(prefix_set))
+    #print("subset: " + str(subset))
+    #print("suffix: " + str(suffix_set))
+    for state in subset:
+        option = 0
+        representative_set_index = -1
+
+        
+        if (state, symbol) in transitions:
+                while (option < 3) and representative_set_index == -1:
+                    if option == 0:
+                        #Check if representative set is in the prefix
+                        representative_set_index = choose_representatives(prefix_set, transitions[(state, symbol)])
+                    elif option == 1:
+                        #Check if representative set is in the suffix
+                        representative_set_index = choose_representatives(suffix_set, transitions[(state, symbol)])
+                    elif option == 2:
+                        #Check if representative set is in the aux_partition (if not, must add a representative)
+                        representative_set_index = choose_representatives(aux_partition, transitions[(state, symbol)])
+
+                    if (representative_set_index == -1):
+                        option += 1
+        else:
+                representative_set_index = -1
+                        
+
+        if representative_set_index == -1:
+            #No hay un subconjunto en el prefijo, el sufijo o la nueva particion de subset
+            #que es representativo del elemento
+            aux_partition.append([state])
+            representative_set_index = len(aux_partition)
+
+        #Id offset
+        if option == 1:
+            representative_set_index += len(prefix_set)
+        elif option == 2:
+            representative_set_index += len(prefix_set) + len(suffix_set)
+
+
+        if not(representative_set_index in states_by_representative):
+            states_by_representative[representative_set_index] = []
+
+        states_by_representative[representative_set_index].append(state)
+
+        #print("Option: " + str(option))
+        #print("representative_set_index: " + str(representative_set_index))
+        #print("states_by_representative: " + str(states_by_representative))
+        sub_partition = []
+        for index, states in states_by_representative.items():
+            sub_partition.append(states)
+
+    #print("====== OUTPUT ======")
+    #print("prefix: " + str(prefix_set))
+    #print("sub_partition: " + str(sub_partition))
+    #print("suffix: " + str(suffix_set))
+    return sub_partition
+
+
+def is_in_subset(subset, alphabet, state, transitions):
+	in_subset = True
+	for symbol in alphabet:
+		in_subset = in_subset and (transitions[(state, symbol)] in subset)
+
+	return in_subset
+
+def eliminate_death_states(subset, alphabet, transitions, terminal_states):
+    result = []
+    while len(subset) > 0:
+        state = subset.pop(0)
+        if not(is_death_state(state, alphabet, transitions, terminal_states)):
+            result.append(state)
+
+    return result
+
+
+def is_death_state(state, alphabet, transitions, terminal_states):
+    another_state = False
+    for symbol in alphabet:
+        if (state, symbol) in transitions:
+            another_state = another_state or (transitions[(state, symbol)] != state)
+
+    return not(another_state) and not(state in terminal_states)
+
+#The representative of a disjoint set is the disjoint set index in the
+#new partition
+def choose_representatives(partition, state):
+    index = 0
+    found = False
+    while index < len(partition) and not(found):
+        subset = partition[index]
+        if not(state in subset):
+            index += 1
+        else:
+            found = True
+
+
+    if not(found):
+        index = -1
+
+    return index
+
+#def choose_representatives_by_dest(partition, symbol, transitions, state):
+#    dest_state = transitions[(state, symbol)]
+#    index = 0
+#    found = False
+#    while index < len(partition) and not(found):
+#        subset = partition[index]
+#        if not(dest_state in subset):
+#            index += 1
+#        else:
+#            found = True
+
+#    if not(found):
+#        index = -1
+
+#    return index
+
+
+def partition_transitions(partition, alphabet, transitions):
+    new_transitions = {}
+    #print("transitions: " + str(transitions))
+    #print("partition: " + str(partition))
+    for subset in partition:
+        #print(str(subset))
+        for state in subset:
+            #print(str(state))
+            for symbol in alphabet:
+                if (state, symbol) in transitions:
+                    #Solo si existia originalmente la transicion
+                
+                    orig_state = choose_representatives(partition, state)
+                    #print("state: " + str(state) + ", orig_state: " + str(orig_state))
+                    #print("transitions: " + str(transitions))
+                    #print("(state, symbol)" + str( (state, symbol) ) )
+                    #print("transitions[(state, symbol)]" + str(transitions[(state, symbol)]))
+                    dest_state = choose_representatives(partition, transitions[(state, symbol)])
+                    if dest_state > -1:
+                        #print("transitions[(state, symbol)]: " + str(transitions[(state, symbol)]) + ", dest_state: " + str(dest_state))
+                        new_transitions[(orig_state, symbol)] = dest_state
+
+    return new_transitions
+
+
+
 
 
 
 def escribir_archivo(automata, filename):
-	f = open(filename, 'w')
+        f = open(filename, 'w')
 
-	for i in range(0, len(automata.estados)):
-		if i < len(automata.estados)-1 :
-			f.write('q')
-			f.write(str(automata.estados[i]))
-			f.write(' ')
-		if i == len(automata.estados)-1 :
-			f.write('q')
-			f.write(str(automata.estados[i]))
+        for i in range(0, len(automata.estados)):
+                if i < len(automata.estados)-1 :
+                        f.write('q')
+                        f.write(str(automata.estados[i]))
+                        f.write(' ')
+                if i == len(automata.estados)-1 :
+                        f.write('q')
+                        f.write(str(automata.estados[i]))
+        f.write('\n')
 
-	f.write('\n')
+        for i in range(0, len(automata.alfabeto)):
+                if (i < len(automata.alfabeto)-1) and (automata.alfabeto[i] != 'lambda') :
+                        f.write(str(automata.alfabeto[i]))
+                        f.write(' ')
+                if (i == len(automata.alfabeto)-1) and (automata.alfabeto[i] != 'lambda') :
+                        f.write(str(automata.alfabeto[i]))
 
-	for i in range(0, len(automata.alfabeto)):
-		if (i < len(automata.alfabeto)-1) and (automata.alfabeto[i] != 'lambda') :
-			f.write(str(automata.alfabeto[i]))
-			f.write(' ')
-		if (i == len(automata.alfabeto)-1) and (automata.alfabeto[i] != 'lambda') :
-			f.write(str(automata.alfabeto[i]))
+        f.write('\n')
 
-	f.write('\n')
+        f.write('q')
+        f.write(str(automata.estado_inicial))
 
-	f.write('q')
-	f.write(str(automata.estado_inicial))
+        f.write('\n')
 
-	f.write('\n')
+        for i in range(0, len(automata.estados_finales)):
+                if i < len(automata.estados_finales)-1 :
+                        f.write('q')
+                        f.write(str(automata.estados_finales[i]))
+                        f.write(' ')
+                if i == len(automata.estados_finales)-1 :
+                        f.write('q')
+                        f.write(str(automata.estados_finales[i]))
 
-	for i in range(0, len(automata.estados_finales)):
-		if i < len(automata.estados_finales)-1 :
-			f.write('q')
-			f.write(str(automata.estados_finales[i]))
-			f.write(' ')
-		if i == len(automata.estados_finales)-1 :
-			f.write('q')
-			f.write(str(automata.estados_finales[i]))
+        f.write('\n')
 
-	f.write('\n')
+        for i in range(0, len(automata.transiciones)):
+                if i < len(automata.transiciones)-1 :
+                        f.write('q')
+                        f.write(str(automata.transiciones[i][0]))
+                        f.write(' ')
+                        f.write(str(automata.transiciones[i][1]))
+                        f.write(' ')
+                        f.write('q')
+                        f.write(str(automata.transiciones[i][2]))
+                        f.write('\n')
+                if i == len(automata.transiciones)-1 :
+                        f.write('q')
+                        f.write(str(automata.transiciones[i][0]))
+                        f.write(' ')
+                        f.write(str(automata.transiciones[i][1]))
+                        f.write(' ')
+                        f.write('q')
+                        f.write(str(automata.transiciones[i][2]))
 
-	for i in range(0, len(automata.transiciones)):
-		if i < len(automata.transiciones)-1 :
-			f.write('q')
-			f.write(str(automata.transiciones[i][0]))
-			f.write(' ')
-			f.write(str(automata.transiciones[i][1]))
-			f.write(' ')
-			f.write('q')
-			f.write(str(automata.transiciones[i][2]))
-			f.write('\n')
-		if i == len(automata.transiciones)-1 :
-			f.write('q')
-			f.write(str(automata.transiciones[i][0]))
-			f.write(' ')
-			f.write(str(automata.transiciones[i][1]))
-			f.write(' ')
-			f.write('q')
-			f.write(str(automata.transiciones[i][2]))
+        f.close()
 
-	f.close()
-
-	return 0
+        return 0
 
 
