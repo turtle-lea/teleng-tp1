8affef9c14c390e8293e5943371687e76ff1a746
Turtle lea <leandro.matayoshi@gmail.com>
2015-06-25 23:37:04 -0300

    Borre comentarios del codigo

--- python/ejercicio_f.py
+++ python/ejercicio_f.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 
 from automata import *
@@ -6,18 +6,14 @@ from automata import *
 def equivalentes(archivo_automata1, archivo_automata2):
 	automata1 = parsear_automata(archivo_automata1)
 	automata2 = parsear_automata(archivo_automata2)
-	
+
 	union_alfabeto = list(set(automata1.alfabeto + automata2.alfabeto))
-	
+
 	automata1.alfabeto = union_alfabeto
 	automata2.alfabeto = union_alfabeto
-	
+
 	automata3 = interseccion_automatas(automata1,automata2.complemento())
 	automata4 = interseccion_automatas(automata2,automata1.complemento())
-	#automata3 = automata3.minimizar_afd_2()
-	#automata3 = minimizar_afd(automata3)
-	#automata4 = minimizar_afd(automata4)
-	#automata4 = automata4.minimizar_afd_2()
 	if len(automata3.estados_finales) == 0 and len(automata4.estados_finales) == 0:
 		print True
 	else:
